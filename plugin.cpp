429b6833a991cc640d033914b0ee5c34e9274296
goujon <thierry.steebierg@gmail.com>
2022-03-10 00:03:11 +0100

    General code revision (cleanup, error handling)

--- plugin.cpp
+++ plugin.cpp
@@ -12,14 +12,10 @@
 #include <map>
 #include <future>
 
-
 #include "server_ws.hpp"
-
 #include "nlohmann/json.hpp"
 
-
 #define FLAG(expr, flag) ((expr) & (flag) ? true : false)
-
 
 using namespace std::literals::string_literals;
 using namespace nlohmann;
@@ -27,8 +23,6 @@ using namespace nlohmann;
 using WsServer = SimpleWeb::SocketServer<SimpleWeb::WS>;
 using Endpoint = SimpleWeb::SocketServerBase<SimpleWeb::WS>::Endpoint;
 using WsConnection = std::shared_ptr< WsServer::Connection >;
-
-
 
 class WebsocketApiPlugin : public MumblePlugin {
 
@@ -38,14 +32,10 @@ public:
 	static constexpr const char *PLUGIN_DESCRIPTION =
 		"Websocket API for receiving events and querying client state";
 
-	static constexpr const char *LOG_PATH = "R:\\mumble.log";
-
-	static constexpr const short WS_PORT          = 8080;
-	static constexpr const char *WS_ENDPOINT_PATH = "mumble_events";
+	static constexpr const unsigned short WS_PORT = 51966;
+	static constexpr const char* WS_ENDPOINT_PATH = "mumble_events";
 
 private:
-	std::ofstream logs;
-
 	bool synchronized = false;
 
 	Endpoint *wsEndpoint;
@@ -53,145 +43,180 @@ private:
 	std::promise< unsigned short > wsServerPort;
 	std::thread *wsServerThread;
 
-	void (*wsErrorHandler)(WebsocketApiPlugin* plugin, const SimpleWeb::error_code &ec) =
-		[](WebsocketApiPlugin* plugin, const SimpleWeb::error_code &ec) {
-			if (ec) {
-				plugin->getLogs() << "Server: Error sending message. " <<
-					// See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for
-					// error code meanings
-					"Error: " << ec << ", error message: " << ec.message() << std::endl;
-			}
-		};
-	/*
-		USAGE:
-	 
-		connection->send(out_message, [this](const SimpleWeb::error_code &ec) {
-			wsErrorHandler(this, ec);
-		});	
-	*/
 public:
 	WebsocketApiPlugin() : MumblePlugin(PLUGIN_NAME, PLUGIN_AUTHOR, PLUGIN_DESCRIPTION) {
-		logs.rdbuf()->pubsetbuf(0, 0);
-		logs.open(LOG_PATH, std::ofstream::out | std::ofstream::app);
-
-		wsInit();
 	}
 
 	~WebsocketApiPlugin() {
 		wsDisconnect();
-		logs.close();
+	}
+
+	mumble_error_t init() noexcept {
+		wsInit();
+		return MUMBLE_STATUS_OK;
 	}
 
 	bool isAlive(mumble_connection_t connection) {
 		return (connection >= 0 && synchronized);
 	}
 
-	std::ofstream& getLogs() {
-		return logs;
-	}
-	
 	virtual void onServerSynchronized(mumble_connection_t connection) noexcept override {
-		synchronized = true;
-		wsBroadcast(jsonMessage("event/connected"));
+		try {
+			synchronized = true;
+			wsBroadcast(jsonMessage("event/connected"));
+		}
+		catch (MumbleAPIException e) {
+			handleApiException("onServerSynchronized", e);
+		}
 	}
 
 	virtual void onServerDisconnected(mumble_connection_t connection) noexcept override {
-		synchronized = false;
-		wsBroadcast(jsonMessage("event/disconnected"));
+		try {
+			synchronized = false;
+			wsBroadcast(jsonMessage("event/disconnected"));
+		}
+		catch (MumbleAPIException e) {
+			handleApiException("onServerDisconnected", e);
+		}
 	}
 
 	virtual void onChannelEntered(mumble_connection_t connection, mumble_userid_t userID,
 								  mumble_channelid_t previousChannelID, mumble_channelid_t newChannelID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg    = jsonMessage("event/channel_entered");
+			msg["user"] = jsonGetUser(connection, userID);
+			if (previousChannelID >= 0) {
+				msg["fromChannel"] = jsonGetChannel(connection, previousChannelID);
+			} else {
+				msg["fromChannel"] = nullptr;
+			}
+			msg["toChannel"] = jsonGetChannel(connection, newChannelID);
+			wsBroadcast(msg);
 		}
-		auto msg    = jsonMessage("event/channel_entered");
-		msg["user"] = jsonGetUser(connection, userID);
-		if (previousChannelID >= 0) {
-			msg["fromChannel"] = jsonGetChannel(connection, previousChannelID);
-		} else {
-			msg["fromChannel"] = nullptr;
+		catch (MumbleAPIException e) {
+			handleApiException("onChannelEntered", e);
 		}
-		msg["toChannel"] = jsonGetChannel(connection, newChannelID);
-		wsBroadcast(msg);
 	}
 	
 	virtual void onChannelExited(mumble_connection_t connection, mumble_userid_t userID,
 								 mumble_channelid_t channelID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg       = jsonMessage("event/channel_exited");
+			msg["user"]    = jsonGetUser(connection, userID);
+			msg["channel"] = jsonGetChannel(connection, channelID);
+			wsBroadcast(msg);
 		}
-		auto msg       = jsonMessage("event/channel_exited");
-		msg["user"]    = jsonGetUser(connection, userID);
-		msg["channel"] = jsonGetChannel(connection, channelID);
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onChannelExited", e);
+		}
 	}
 
 	virtual void onUserMuteDeafStateChanged(mumble_connection_t connection, mumble_userid_t userID,
 											mumble_mutedeaf_state_t muteDeafState) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg    = jsonMessage("event/user_mute_deafen_state_changed");
+			msg["user"] = jsonGetUser(connection, userID);
+			fillMuteDeafState(msg, muteDeafState);
+			wsBroadcast(msg);
 		}
-		auto msg    = jsonMessage("event/user_mute_deafen_state_changed");
-		msg["user"] = jsonGetUser(connection, userID);
-		fillMuteDeafState(msg, muteDeafState);
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onUserMuteDeafStateChanged", e);
+		}
 	}
 
 	virtual void onUserTalkingStateChanged(mumble_connection_t connection, mumble_userid_t userID,
 										   mumble_talking_state_t talkingState) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg            = jsonMessage("event/user_talking_state_changed");
+			msg["user"]         = jsonGetUser(connection, userID);
+			msg["talkingState"] = talkingState;
+			wsBroadcast(msg);
 		}
-		auto msg            = jsonMessage("event/user_talking_state_changed");
-		msg["user"]         = jsonGetUser(connection, userID);
-		msg["talkingState"] = talkingState;
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onUserTalkingStateChanged", e);
+		}
 	}
 
 	virtual void onUserAdded(mumble_connection_t connection, mumble_userid_t userID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg    = jsonMessage("event/user_added");
+			msg["user"] = jsonGetUser(connection, userID);
+			wsBroadcast(msg);
 		}
-		auto msg    = jsonMessage("event/user_added");
-		msg["user"] = jsonGetUser(connection, userID);
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onUserAdded", e);
+		}
 	}
 
 	virtual void onUserRemoved(mumble_connection_t connection, mumble_userid_t userID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg  = jsonMessage("event/user_removed");
+			msg["id"] = userID;
+			wsBroadcast(msg);
 		}
-		auto msg  = jsonMessage("event/user_removed");
-		msg["id"] = userID;
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onUserRemoved", e);
+		}
 	}
 
 	virtual void onChannelAdded(mumble_connection_t connection, mumble_channelid_t channelID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg = jsonMessage("event/channel_added");
+			msg["channel"] = jsonGetChannel(connection, channelID);
+			wsBroadcast(msg);
 		}
-		auto msg = jsonMessage("event/channel_added");
-		msg["channel"] = jsonGetChannel(connection, channelID);
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onChannelAdded", e);
+		}
 	}
 
 	virtual void onChannelRemoved(mumble_connection_t connection, mumble_channelid_t channelID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg  = jsonMessage("event/channel_removed");
+			msg["id"] = channelID;
+			wsBroadcast(msg);
 		}
-		auto msg  = jsonMessage("event/channel_removed");
-		msg["id"] = channelID;
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onChannelRemoved", e);
+		}
 	}
 
 	virtual void onChannelRenamed(mumble_connection_t connection, mumble_channelid_t channelID) noexcept override {
-		if (!isAlive(connection)) {
-			return;
+		try {
+			if (!isAlive(connection)) {
+				return;
+			}
+			auto msg       = jsonMessage("event/channel_renamed");
+			msg["channel"] = jsonGetChannel(connection, channelID);
+			wsBroadcast(msg);
 		}
-		auto msg       = jsonMessage("event/channel_renamed");
-		msg["channel"] = jsonGetChannel(connection, channelID);
-		wsBroadcast(msg);
+		catch (MumbleAPIException e) {
+			handleApiException("onChannelRenamed", e);
+		}
 	}
 
 	virtual void releaseResource(const void *ptr) noexcept override {
@@ -202,49 +227,64 @@ public:
 
 	void handleMessage(WsConnection connection, json &msg) {
 		auto type = msg["type"].get< std::string >();
-		if (type.compare("request/local_user_id") == 0) {
-			wsSendLocalUserId(connection);
-		} else if (type.compare("request/local_user_state") == 0) {
-			wsSendLocalUserState(connection);
-		} else if (type.compare("request/channels") == 0) {
-			wsSendChannels(connection);
-		} else if (type.compare("request/change_channel") == 0) {
-			handleChangeChannel(msg["channelId"].get< mumble_channelid_t >());
-		} else if (type.compare("request/is_connected") == 0) {
-			auto msg  = jsonMessage("response/is_connected");
-			msg["connected"] = synchronized;
-			wsSendJson(connection, msg);
-		} else if (type.compare("request/set_muted") == 0) {
-			m_api.requestLocalUserMute(msg["muted"]);
-			auto msg = jsonMessage("response/set_muted");
-			wsSendJson(connection, msg);
-		} else if (type.compare("request/set_deafened") == 0) {
-			m_api.requestLocalUserDeaf(msg["deafened"]);
-			m_api.requestLocalUserMute(msg["deafened"]);
-			auto msg = jsonMessage("response/set_deafened");
-			wsSendJson(connection, msg);
-		} else if (type.compare("request/user_mute_deafen_state") == 0) {
-			wsSendUserMuteDeafState(connection, msg["id"].get< mumble_userid_t >());
-		} else if (type.compare("request/set_local_mute") == 0) {
-			handleSetLocalMute(msg["id"].get< mumble_userid_t >(), msg["enable"].get< bool >());
+		try {
+			if (type.compare("request/local_user_id") == 0) {
+				wsSendLocalUserId(connection);
+			} else if (type.compare("request/local_user_state") == 0) {
+				wsSendLocalUserState(connection);
+			} else if (type.compare("request/channels") == 0) {
+				wsSendChannels(connection);
+			} else if (type.compare("request/change_channel") == 0) {
+				handleChangeChannel(msg["channelId"].get< mumble_channelid_t >());
+			} else if (type.compare("request/is_connected") == 0) {
+				auto msg  = jsonMessage("response/is_connected");
+				msg["connected"] = synchronized;
+				wsSendJson(connection, msg);
+			} else if (type.compare("request/set_muted") == 0) {
+				m_api.requestLocalUserMute(msg["muted"]);
+				auto msg = jsonMessage("response/set_muted");
+				wsSendJson(connection, msg);
+			} else if (type.compare("request/set_deafened") == 0) {
+				m_api.requestLocalUserDeaf(msg["deafened"]);
+				m_api.requestLocalUserMute(msg["deafened"]);
+				auto msg = jsonMessage("response/set_deafened");
+				wsSendJson(connection, msg);
+			} else if (type.compare("request/user_mute_deafen_state") == 0) {
+				wsSendUserMuteDeafState(connection, msg["id"].get< mumble_userid_t >());
+			} else if (type.compare("request/set_local_mute") == 0) {
+				handleSetLocalMute(msg["id"].get< mumble_userid_t >(), msg["enable"].get< bool >());
+			}
+		}
+		catch (MumbleAPIException e) {
+			handleApiException("handleMessage[" + type + "]", e);
 		}
 	}
 
 	void handleChangeChannel(mumble_channelid_t channelID) {
-		mumble_connection_t connection = m_api.getActiveServerConnection();
-		if (!isAlive(connection)) {
-			return;
+		try {
+			mumble_connection_t connection = m_api.getActiveServerConnection();
+			if (!isAlive(connection)) {
+				return;
+			}
+			mumble_userid_t userID = m_api.getLocalUserID(connection);
+			m_api.requestUserMove(connection, userID, channelID);
 		}
-		mumble_userid_t userID = m_api.getLocalUserID(connection);
-		m_api.requestUserMove(connection, userID, channelID);
+		catch (MumbleAPIException e) {
+			handleApiException("handleChangeChannel", e);
+		}
 	}
 
 	void handleSetLocalMute(mumble_userid_t userID, bool enable) {
-		mumble_connection_t connection = m_api.getActiveServerConnection();
-		if (!isAlive(connection)) {
-			return;
+		try {
+			mumble_connection_t connection = m_api.getActiveServerConnection();
+			if (!isAlive(connection)) {
+				return;
+			}
+			m_api.requestLocalMute(connection, userID, enable);
 		}
-		m_api.requestLocalMute(connection, userID, enable);
+		catch (MumbleAPIException e) {
+			handleApiException("handleSetLocalMute", e);
+		}
 	}
 
 private:
@@ -301,12 +341,61 @@ private:
 	// Web Sockets
 	//
 
+	void wsInit() {
+		wsServer.config.port = WS_PORT;
+		wsEndpoint = &wsServer.endpoint["^/"s + WS_ENDPOINT_PATH + "/?$"s];
+
+		wsEndpoint->on_message = [this](WsConnection connection, std::shared_ptr< WsServer::InMessage > in_message) {
+			if (!synchronized) {
+				return;
+			}
+
+			try {
+				auto input = json::parse(in_message->string());
+				if (input.contains("type")) {
+					handleMessage(connection, input);
+				}
+			}
+			catch (...) {
+				throw MumbleAPIException(MUMBLE_EC_GENERIC_ERROR, "Could not process WS JSON message:\n"s + in_message->string());
+			}
+
+			auto out_message = in_message->string();
+		};
+
+		// Can modify handshake response headers here if needed
+		wsEndpoint->on_handshake = [](WsConnection /*connection*/,
+			SimpleWeb::CaseInsensitiveMultimap& /*response_header*/) {
+				return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
+		};
+
+		// See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code
+		// meanings
+		wsEndpoint->on_error = [this](WsConnection connection, const SimpleWeb::error_code& ec) {
+			wsErrorHandler(this, ec);
+		};
+
+		wsServerThread = new std::thread([this]() {
+			wsServer.start([this](unsigned short port) {
+				wsServerPort.set_value(port);
+			});
+		});
+
+		m_api.log(("Server listening on port "s + std::to_string(wsServerPort.get_future().get())).c_str());
+	}
+
+	void wsDisconnect() {
+		wsServer.stop();
+	}
+
 	void wsBroadcast(std::string& message) {
 		if (wsEndpoint == NULL) {
 			return;
 		}
 		for (const auto conn : wsEndpoint->get_connections()) {
-			conn.get()->send(message);
+			conn.get()->send(message, [this](const SimpleWeb::error_code& ec) {
+				wsErrorHandler(this, ec);
+			});
 		}
 	}
 
@@ -315,7 +404,9 @@ private:
 	}
 
 	void wsSendJson(WsConnection wsConnection, json &json) {
-		wsConnection->send(json.dump());
+		wsConnection->send(json.dump(), [this](const SimpleWeb::error_code& ec) {
+			wsErrorHandler(this, ec);
+		});
 	}
 
 	void wsSendLocalUserId(WsConnection wsConnection) {
@@ -345,48 +436,6 @@ private:
 		wsSendJson(wsConnection, msg);
 	}
 
-	void wsInit() {
-		wsServer.config.port = WS_PORT;
-		wsEndpoint           = &wsServer.endpoint["^/"s + WS_ENDPOINT_PATH + "/?$"s];
-
-		wsEndpoint->on_message = [this](WsConnection connection, std::shared_ptr< WsServer::InMessage > in_message) {
-			if (!synchronized) {
-				return;
-			}
-
-			try {
-				auto input = json::parse(in_message->string());
-				if (input.contains("type")) {
-					handleMessage(connection, input);
-				}
-			} catch (...) {
-				logs << "Could not process WS JSON message:" << std::endl << in_message;
-			}
-
-            auto out_message = in_message->string();
-		};
-
-		// Can modify handshake response headers here if needed
-		wsEndpoint->on_handshake = [](WsConnection /*connection*/,
-							   SimpleWeb::CaseInsensitiveMultimap & /*response_header*/) {
-			return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
-		};
-
-		// See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code
-		// meanings
-		wsEndpoint->on_error = [this](WsConnection connection, const SimpleWeb::error_code &ec) {
-			logs << "Server: Error in connection " << connection.get() << ". "
-				 << "Error: " << ec << ", error message: " << ec.message() << std::endl;
-		};
-
-		wsServerThread = new std::thread([this]() {
-			// Start server
-			wsServer.start([this](unsigned short port) { wsServerPort.set_value(port); });
-		});
-
-		logs << "Server listening on port " << wsServerPort.get_future().get() << std::endl << std::endl;
-	}
-
 	void fillMuteDeafState(json &msg, mumble_mutedeaf_state_t muteDeafState) {
 		msg["muted"]           = FLAG(muteDeafState, MDS_MUTE);
 		msg["deafened"]        = FLAG(muteDeafState, MDS_DEAF);
@@ -399,28 +448,38 @@ private:
 		msg["recording"]       = FLAG(muteDeafState, MDS_RECORDING);
 	}
 
-	void wsDisconnect() {
-		wsServer.stop();
-	}
-
 	//
 	// MISC
 	//
 
+	void handleApiException(const std::string& where, const MumbleAPIException& e) const {
+		m_api.log((where + ": ("s + std::to_string(e.errorCode()) + ") "s + e.what()).c_str());
+	}
+
+	void (*wsErrorHandler)(WebsocketApiPlugin* plugin, const SimpleWeb::error_code& ec) =
+		[](WebsocketApiPlugin* plugin, const SimpleWeb::error_code& ec) {
+		if (ec) {
+			throw MumbleAPIException(MUMBLE_EC_GENERIC_ERROR,
+				"WebSocket connection error ("s + std::to_string(ec.value()) + "): "s + ec.message());
+		}
+	};
+
 	virtual void dumpState(mumble_connection_t connection) noexcept {
+		std::ostringstream ss;
 		try {
-			logs << "Server " << m_api.getServerHash(connection) << " finished synchronizing" << std::endl;
+			ss << "Server " << m_api.getServerHash(connection) << " finished synchronizing" << std::endl;
 			std::map< mumble_channelid_t, std::vector< mumble_userid_t > > channelUsers;
 			for (auto userId : m_api.getAllUsers(connection)) {
 				const auto channelId = m_api.getChannelOfUser(connection, userId);
 				channelUsers[channelId].push_back(userId);
 			}
 			for (auto channelId : m_api.getAllChannels(connection)) {
-				logs << m_api.getChannelName(connection, channelId) << std::endl;
+				ss << m_api.getChannelName(connection, channelId) << std::endl;
 				for (auto userId : channelUsers[channelId]) {
-					logs << "  - " << m_api.getUserName(connection, userId) << std::endl;
+					ss << "  - " << m_api.getUserName(connection, userId) << std::endl;
 				}
 			}
+			m_api.log(ss.str().c_str());
 		} catch (const MumbleAPIException &e) {
 			std::cerr << "dumpState: " << e.what() << " (ErrorCode " << e.errorCode() << ")" << std::endl;
 		}
